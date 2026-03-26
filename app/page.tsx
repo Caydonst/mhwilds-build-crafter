c77196eb5c814889844a3a5199c069d96b2b5c2d
Caydon Thomas <caydons.thomas@gmail.com>
2026-03-26 00:13:40 -0400

    Updated landing page UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import React, {useEffect, useState} from "react";
 import Builder from "@/app/components/builder/builder"
 import fetchAll from "@/app/api/apiCalls/apiCalls"
 import {Weapon, ArmorBySlot, CharmRank, Decoration, Skill} from "@/app/api/types/types"
-import {SparklesIcon, ArrowRightIcon, WrenchScrewdriverIcon} from "@heroicons/react/24/solid"
+import {SparklesIcon, ChevronRightIcon, WrenchScrewdriverIcon, CubeTransparentIcon} from "@heroicons/react/24/solid"
+import {CogIcon} from "@heroicons/react/24/outline"
 import Link from "next/link"
 
 export default function Home() {
@@ -14,21 +15,16 @@ export default function Home() {
         <main className={styles.homeContainer}>
             <div className={styles.mainContainer}>
                 <div className={styles.titleContainer}>
-                    <h1>Monster Hunter Wilds</h1>
-                    <h2>Builder</h2>
+                    <h2>Monster Hunter Wilds</h2>
+                    <h1><CogIcon className={styles.titleIcon} />Builder</h1>
                 </div>
+                <p>An advanced build crafter that allows for optimized builds through real-time stat calculation.</p>
+                <Link href="/builder" className={styles.buildGeneratorBtn}>
+                    <div>Open builder<ChevronRightIcon className={styles.arrowIcon}/></div>
+                </Link>
                 <div className={styles.tileContainer}>
-                    <Link href="/builder" className={styles.buildGeneratorBtn}>
-                        <div className={styles.tileHeader}>
-                            <div className={styles.tileIconContainer}>
-                                <WrenchScrewdriverIcon className={styles.sparklesIcon} />
-                            </div>
-                            <h3>Builder</h3>
-                        </div>
-                        <p>An advanced build crafter that allows for optimized builds through real-time stat calculation.</p>
-                        <ArrowRightIcon className={styles.arrowIcon}/>
-                    </Link>
-                    <div className={styles.buildGeneratorBtn}>
+
+                    {/*<div className={styles.buildGeneratorBtn}>
                         <div className={styles.tileHeader}>
                             <div className={styles.tileIconContainer}>
                                 <SparklesIcon className={styles.sparklesIcon} />
@@ -36,7 +32,7 @@ export default function Home() {
                             <h3>Build Generator</h3>
                         </div>
                         <div className={styles.comingSoon}>Coming Soon</div>
-                    </div>
+                    </div>*/}
                 </div>
             </div>
         </main>
